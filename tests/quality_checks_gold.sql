0438f87f2c1f7296eb2fe7ca375a2bd97b4a5dc2
DILANSH JAIN <dilanshsjain2005@gmail.com>
2026-08-05 23:07:56 +0530

    Update quality_checks_gold.sql

--- tests/quality_checks_gold.sql
+++ tests/quality_checks_gold.sql
@@ -10,3 +10,16 @@ SELECT
 FROM gold.dim_customers
 GROUP BY customer_key
 HAVING COUNT(*) > 1;
+
+
+-- ====================================================================
+-- Checking 'gold.product_key'
+-- ====================================================================
+-- Check for Uniqueness of Product Key in gold.dim_products
+-- Expectation: No results 
+SELECT 
+    product_key,
+    COUNT(*) AS duplicate_count
+FROM gold.dim_products
+GROUP BY product_key
+HAVING COUNT(*) > 1;
