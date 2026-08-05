c0a7f9812dc41c7575eaf19c5150aa82453d4f49
DILANSH JAIN <dilanshsjain2005@gmail.com>
2026-08-05 23:29:06 +0530

    Remove redundant view drop check for dim_customers
    Removed duplicate check for existing view before dropping it.

--- scripts/Gold/ddl_gold.sql
+++ scripts/Gold/ddl_gold.sql
@@ -20,8 +20,8 @@ Usage:
 
 IF OBJECT_ID('gold.dim_customers', 'V') IS NOT NULL
     DROP VIEW gold.dim_customers;
-IF OBJECT_ID('gold.dim_customers', 'V') IS NOT NULL
-    DROP VIEW gold.dim_customers;
+
+
 GO
 
 CREATE VIEW gold.dim_customers AS
